36beb47dc18a97e399c4a3a5e0eb2175e4d7d2fc
Cody Switzer <cswitzer85@gmail.com>
2020-01-25 17:31:09 -0600

    8th commit; var buildList becomes list of acceptible characters per user inputs, foundation for password string commented out at bottom of js page

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+// *save point here*
+
 //---------------_______________---------------Variables---------------_______________---------------
-var lowerSelected
+var lowerSelected;
 var capitalSelected;
 var numbersSelected;
 var specialSelected;
 var characterLengthSelected;
 var characterLength;
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "`", "_", "-", "+", "\=", "{", "[", "\'", "]", "}", "\\", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?"];
 var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "`", "_", "-", "+", "\=", "{", "[", "\'", "]", "}", "\\", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?"];
+var buildList;
 //---------------_______________---------------_______________---------------_______________---------------
-
 
 
 //needed variables based on html but not yet functional or tested???
@@ -38,7 +40,7 @@ function selectPasswordLength() {
 //------------------------------------------------------------------*May delete and make lower-case the default*-------
 function lowerInclude() {
   lowerSelected = null;
-  if (lowerSelected !== true || false); {
+  if (lowerSelected !== true); {
     lowerSelected = confirm("Would you like to include lower-case letters? ok = yes, cancel = no)");
     //$$$$$*****changes value of calEl to ture or false, based on the users prompt selection*****$$$$$
     if (lowerSelected !== true) {
@@ -49,7 +51,7 @@ function lowerInclude() {
 
 function capitalsInclude() {
   capitalSelected = null;
-  if (capitalSelected !== true || false); {
+  if (capitalSelected !== true); {
     capitalSelected = confirm("Would you like to include capital letters? ok = yes, cancel = no)");
     //$$$$$*****changes value of calEl to ture or false, based on the users prompt selection*****$$$$$
     if (capitalSelected !== true) {
@@ -60,7 +62,7 @@ function capitalsInclude() {
 
 function numbersInclude() {
   numbersSelected = null;
-  if (numbersSelected !== true || false); {
+  if (numbersSelected !== true); {
     numbersSelected = confirm("Would you like to include numbers? ok = yes, cancel = no)");
     //$$$$$*****changes value of calEl to ture or false, based on the users prompt selection*****$$$$$
     if (numbersSelected !== true) {
@@ -71,7 +73,7 @@ function numbersInclude() {
 
 function specialInclude() {
   specialSelected = null;
-  if (specialSelected !== true || false); {
+  if (specialSelected !== true); {
     specialSelected = confirm("Would you like to include special characters? ok = yes, cancel = no)");
     //$$$$$*****changes value of calEl to ture or false, based on the users prompt selection*****$$$$$
     if (specialSelected !== true) {
@@ -80,24 +82,65 @@ function specialInclude() {
   }
 } // end of capitalsIncluded function
 
+function build(){
+  
+  buildList = [];
+  
+  alert(" you requested a password with " + characterLength + " characters \n which includes... \n lower case letters: " + lowerSelected + "\n capital letters: " + capitalSelected + "\n numbers: " + numbersSelected + "\n and special characters: " + specialSelected);
+  
+  if (lowerSelected === true){
+      buildList = lowerLetters;
+  }
+  if (capitalSelected === true){
+    if (buildList.length === 0) {
+      buildList = capitalLetters;
+    } else {
+      buildList = buildList.concat(capitalLetters);
+    }
+  }
+  if (numbersSelected === true){
+    if (buildList.length === 0) {
+      buildList = numbers;
+    } else {
+      buildList = buildList.concat(numbers);
+    }
+  }
+  if (specialSelected === true){
+    if (buildList.length === 0) {
+      buildList = specialCharacters;
+    } else {
+      buildList = buildList.concat(specialCharacters);
+    }
+  }
+  
+  else if (lowerSelected === false && capitalSelected === false && numbersSelected === false && specialSelected === false){
+    alert("Cannot create a password without any characters selected, please try again.")
+  }
+  }
 
 
-
+//function uses true/false status to build new array from user selected criteria
 function createPassword() {
   selectPasswordLength();
   lowerInclude();
   capitalsInclude();
   numbersInclude();
   specialInclude();
+
+  
+  
+  
+  
+  
+  
+  
+  
   build();
 } //end of create password function
 
 
-function build(){
-  alert(" you requested a password with " + characterLength + " characters \n which includes... \n lower case letters: " + lowerSelected + "\n capital letters: " + capitalSelected + "\n numbers: " + numbersSelected + "\n and special characters: " + specialSelected);
   
   
-  var buildList = []
   // // if """function"""Selected is true, add to list of critera; if false, move on
   // // display criteria to user for confirmation
   // // run loop to get random selection from list of true selections to run the same number of times as the character length defined by user
@@ -105,7 +148,7 @@ function build(){
   //   var randomCharacter = Math.floor(Math.random() * 5) + 1; //-----------------exchange "5" for a variable based on list length
   //     console.log(randomCharacter);
   // // render resulting password to the password section of the html file
-}
+
 
 
 //$$$$$*****when generate button is clicked, password generation is initialized*****$$$$$
@@ -125,3 +168,16 @@ generateEl.addEventListener("click", function (event) {
   );
 }
 )
+/*
+      var passwordResult = '';
+      //creates a random number the same length as what the user requested for password length
+      for (var i = 0; i < characterLength; i++) {
+        var num = Math.floor(Math.random() * 10) + 1;
+        passwordResult = passwordResult + (num);
+        
+        console.log(passwordResult);
+      }
+
+
+
+*/
