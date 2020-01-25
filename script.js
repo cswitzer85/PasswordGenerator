14608b06b32d93e6150fa7da94226b62666ce11f
Cody Switzer <cswitzer85@gmail.com>
2020-01-25 00:19:50 -0600

    3rd commit; variables added, on-click and initial functions are working

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//---------------_______________---------------Variables---------------_______________---------------
+var capSelected;
+var nextTag;
+var characterLengthSelected;
+var characterLength;
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "`", "_", "-", "+", "\=", "{", "[", "\'", "]", "}", "\\", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "f", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//---------------_______________---------------_______________---------------_______________---------------
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+
+//needed variables based on html but not yet functional or tested???
+
+var generateEl = document.querySelector("#generate");
+var passwordEl = document.querySelector("#password");
+
+//functions
+function selectPasswordLength() {
+  var characterLengthSelected;
+  while (characterLengthSelected !== true) {
+    characterLength = prompt("Please enter the number of characters you would like in the password. (Note: Minimum: 8 & Maximum: 128; there will be a prompt for each.)");
+    //$$$$$*****if the user selects a number between 8 & 128 characterLengthSelecgted is changed to true*****$$$$$
+    if (characterLength >= 8 && characterLength <= 128) {
+      alert(characterLength + " characters it is! Let's go!");
+      characterLengthSelected = true;
+    } else {
+      alert("ERROR Please enter a valid age");
+      characterLengthSelected = undefined;
+    }
+  }
+} //end of lengthSelect function
+
+if (capSelected !== true || false); {
+  function capitalsInclude() {
+    capSelected = confirm("Would you like to include capital letters? ok = yes, cancel = no)");
+    //$$$$$*****changes value of calEl to ture or false, based on the users prompt selection*****$$$$$
+    if (capSelected !== true) {
+      capSelected = false;
+    }
+  }
+} // end of capitalsIncluded function
+
+function createPassword() {
+  selectPasswordLength();
+  capitalsInclude();
+} //end of create password function
+
+
+//$$$$$*****when generate button is clicked, password generation is initialized*****$$$$$
+generateEl.addEventListener("click", function (event) {
+  event.preventDefault(); //may not be needed
+
+  createPassword();
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-// for loop
-// ---> for (var i = 0; i < listName.length; i++) { if... goes here | if else... goes here | else... goes here}
-
-
-// list of special characters... ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "`", "_", "-", "+", "=", "{", "[", "\'", "]", "}", "\\", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?";]
-
-
-//Needed for password...GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria                        ***MULT PROMPTS***
-// WHEN prompted for password criteria                                                       ***1ST PROMPT***
-// THEN I select which criteria to include in the password 
-// WHEN prompted for the length of the password                                              ***2nd PROMPT***
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password                              ***3rd PROMPT***
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt 
-// THEN my input should be validated and at least one character type should be selected      ***4th PROMPT; based on at least 1 selection from PROMPT #3***
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated                                                            ***ALL PROMPTS SUCCESSFUL***
-// THEN the password is either displayed in an alert or written to the page
+)
